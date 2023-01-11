e3ee204fcec4455ce0439e67ec5d7de7c6887122
concepteur19 <zobel.tchomgui@gmail.com>
2023-01-11 11:19:58 +0100

    11-01-23

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,32 +5,35 @@ import "../styles/home.css";
 function Home() {
   return (
     <div>
-      <h1 className="title">Software Engineer</h1>
-      <h2 className="another-title-h2-1">Fullstack web Developper</h2>
-      <h2 className="another-title-h2-2">Junior web Designer</h2>
-      <a href="/contact">
-        <Button
-          textArea="Contact"
-          style={{
-            top: "57.66%",
-            left: "16%",
-          }}
-        />
-      </a>
+      <div className="first-block">
+        <h1 className="title">Software Engineer</h1>
+        <h2 className="another-title-h2-1">Fullstack web Developper</h2>
+        <h2 className="another-title-h2-2">Junior web Designer</h2>
+        <a href="/contact">
+          <Button
+            textArea="Contact"
+            style={{
+              top: "57.66%",
+              left: "16%",
+            }}
+          />
+        </a>
+      </div>
 
-      <h2 className="h22">
-        Hey there I'm <h1 className="h11"> Zobel </h1>
-      </h2>
+      <div className="second-block">
+        <div className="first-line ">Hey there I'm</div>
+        <div className="second-line typewriter">NGUENING</div>
+        <a href="/about">
+          <Button
+            textArea="About"
+            style={{
+              top: "42.63%",
+              left: "68%",
+            }}
+          />
+        </a>
+      </div>
 
-      <a href="/about">
-        <Button
-          textArea="About"
-          style={{
-            top: "37.63%",
-            left: "68%",
-          }}
-        />
-      </a>
     </div>
   );
 }
