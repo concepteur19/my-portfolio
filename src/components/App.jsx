0ce258613fdb3d590093974e78ad74b7bf7a5fd7
concepteur19 <zobel.tchomgui@gmail.com>
2023-01-25 23:07:04 +0100

    light/dark mode persistance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "../styles/App.css";
 
@@ -9,41 +9,44 @@ import Lateralbar from "./Lateralbar";
 import Home from "../pages/home";
 import Projects from "../pages/projects";
 import About from "../pages/about";
-import Contact from "../pages/contact"
+import Contact from "../pages/contact";
 
 function App() {
-
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleLightClick() {
-    setIsDarkMode(!isDarkMode)
+    setIsDarkMode(!isDarkMode);
   }
 
   useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      setIsDarkMode(true);
+    } 
+  }, [])
+
+  useEffect(() => {
     if (isDarkMode) {
-      document.body.classList.remove("light-theme");
       document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
+
     } else {
-      document.body.classList.add("light-theme"); 
       document.body.classList.remove("dark-theme");
+      localStorage.setItem("theme", "light");
     }
   }, [isDarkMode]);
 
   return (
-    <div className={`App ${isDarkMode? 'dark-theme' : 'light-theme' }`} >
-      <Navbar 
-        click = {handleLightClick}
-        ligthOrDarkMode = {isDarkMode}
-      />
+    <div className={`App ${isDarkMode ? "dark-theme" : "light-theme"}`}>
+      <Navbar click={handleLightClick} ligthOrDarkMode={!isDarkMode} />
       <Lateralbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects" element={ <Projects /> } />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />.
         </Routes>
-          
       </BrowserRouter>
     </div>
   );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -49,14 +49,17 @@ function Projects() {
   const [width, setWidth] = useState(0);
   const carousel = useRef();
 
-  useEffect(() =>
-    {setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)}
-    , []
-  );
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
   return (
     <div className="container">
-      <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
         <motion.div
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
