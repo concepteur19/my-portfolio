14ce2cb08b48c59f882336e2df69fcb9fe8f0787
concepteur19 <zobel.tchomgui@gmail.com>
2023-01-25 13:12:10 +0100

    slider +++

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-//import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import images from "../images";
 import ProjectCard from "../components/Project-card";
 
@@ -8,8 +8,7 @@ const projects = [
   {
     name: "My portfolio",
     image: images[0],
-    description:
-      "Work exp, education, projects",
+    description: "Work exp, education, projects",
     link: "https://www.CJS-portfolio.com",
   },
   {
@@ -26,13 +25,43 @@ const projects = [
     description: "My personal Todo list",
     link: "https://www.CJS-Todo.com",
   },
+  {
+    name: "CJS-Todo",
+    image: images[2],
+    description: "My personal Todo list",
+    link: "https://www.CJS-Todo.com",
+  },
+  {
+    name: "CJS-Todo",
+    image: images[2],
+    description: "My personal Todo list",
+    link: "https://www.CJS-Todo.com",
+  },
+  {
+    name: "CJS-Todo",
+    image: images[2],
+    description: "My personal Todo list",
+    link: "https://www.CJS-Todo.com",
+  },
 ];
 
 function Projects() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() =>
+    {setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)}
+    , []
+  );
+
   return (
     <div className="container">
-      <motion.div className="carousel">
-        <motion.div className="inner-carousel">
+      <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
           {projects.map((projectDetails) => (
             <motion.div className="item">
               <ProjectCard
